7b1d6e2bda7a4b42d3c56204fb72fdd20b3253c3
KUMAR Sumit OBS/GDO <cmfr2012@orange.com>
2023-02-04 21:51:52 +0530

    new meessage

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
-provider "google" {
-  project     = "tcb-project-371706"
-  #credentials = file("tcb-project-371706-4c5de465c0d5.json")
-  region      = "us-east1"
+# provider "google" {
+#   project     = "tcb-project-371706"
+#   credentials = file("tcb-project-371706-4c5de465c0d5.json")
+#   region      = "us-east1"
 
-}
+# }
 
-resource "google_compute_network" "vpc1" {
-  name                    = "my-custom-network-1"
-  auto_create_subnetworks = "false"
+# resource "google_compute_network" "vpc1" {
+#   name                    = "my-custom-network-1"
+#   auto_create_subnetworks = "false"
 
-}
+# }
 
-resource "google_compute_subnetwork" "my-custom-subnet1" {
-  name          = "my-custom-subnet-1"
-  ip_cidr_range = "10.255.196.0/24"
-  network       = google_compute_network.vpc1.name
-  region        = "us-east1"
-}
+# resource "google_compute_subnetwork" "my-custom-subnet1" {
+#   name          = "my-custom-subnet-1"
+#   ip_cidr_range = "10.255.196.0/24"
+#   network       = google_compute_network.vpc1.name
+#   region        = "us-east1"
+# }
